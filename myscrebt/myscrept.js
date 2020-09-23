6096ad4626b9a0cddd72f1d5239d5106593fa49b
mahmoudseder <YOUR EMAIL ADDRESS>
2020-09-23 14:37:10 +0300

    jsjs01

---
+++ myscrebt/myscrept.js
@@ -0,0 +1,23 @@
+var hi = prompt("Welcome to my website Enter Your Name");
+
+function supermarket() {
+    
+
+var phoneNumber = prompt("Enter Your Phone  NUmber ?");
+
+ if ( phoneNumber == "079") {
+     alert("Zain !!!");
+    }else if (phoneNumber == "078") {
+    alert("Umniah")
+} else if (phoneNumber == "077" ){
+    alert("Orange!");
+} else{
+    alert("somthing went wrong sorry");
+}
+
+//   }
+
+//   // output
+ document.write('<h3>'+hi+'</h3>');
+}
+supermarket();
